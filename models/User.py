ad3bbe10129b37d678e5ed6686cf21dea5ce2777
Shikkic <Shikkic@gmail.com>
2016-05-20 11:48:27 -0400

    add User and UserForm models

--- models/User.py
+++ models/User.py
@@ -1,13 +1,10 @@
 from mongoengine import *
+from tornado.gen import coroutine
 
 class User(Document):
-    _id = StringField(required=True)
-    first_name = StringField(required=True)
-    last_name = StringField(required=True)
+    _id = StringField(required=True, primaryKey=True)
+    name = StringField(required=True)
     start_up_name = StringField()
     start_up_description = StringField()
-    start_up_tags = ListField()
-
-    @property
-    def full_name(self):
-        return "%s, %s" % (self.last_name, self.first_name)
+    start_up_tags = StringField()
+    onboarding_complete = BooleanField(default=False)
