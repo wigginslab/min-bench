4ebc9ad7a003ea29b603bfd85f4807c54ee24958
Shikkic <Shikkic@gmail.com>
2016-06-15 20:10:32 -0400

    modified VCMatcherHandler to act as API and removing template rendering

--- routes/VCMatcherHandler.py
+++ routes/VCMatcherHandler.py
@@ -14,8 +14,7 @@ class VCMatcherHandler(BaseHandler):
         current_user = self.get_current_user()
         user_email = current_user['email']
         user = yield retrieve_user_with_email_id(user_email)
-
+        # TODO implement soft caching with TTL
         investors = fetch_investors(user);
-        user_start_up_tags = retrieve_start_up_tags_from_user(user)
-
-        self.render("vcmatcher.html", title="VC Matcher", investors=investors, tags=user_start_up_tags)
+        json_resp = {"investors": investors}
+        self.write(json_resp)
